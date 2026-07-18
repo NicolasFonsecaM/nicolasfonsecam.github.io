ea664335e84272f519c3febcfada6ea122067d9a
Nicolas <105525681+NicolasFonsecaM@users.noreply.github.com>
2026-07-18 01:14:46 -0300

    a

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <-- Adicione esta linha exatamente aqui
+  output: 'export',
+  trailingSlash: true, // <-- Adicione esta linha exatamente aqui
   images: {
-    unoptimized: true, // Necessário para o GitHub Pages aceitar imagens tag <img /> sem o servidor Next.js otimizando
+    unoptimized: true,
   },
 };
 
